744dc8845e38ad41c8ab406c75591c889d8d0e1f
Pat <sipapat.prasopsap@gmail.com>
2019-11-15 18:00:05 +0100

    Change jQuery

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     getLanguage();
     showLanguage();
     /* Smooth scroll */
-    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (e) {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
             && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                event.preventDefault();
+                e.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
                 }, 400, 'swing', function () {
