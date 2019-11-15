c59ff0953db09fe018e1e79e4349e85ee2b1c515
Pat <sipapat.prasopsap@gmail.com>
2019-11-15 12:39:19 +0100

    resume

---
+++ database.php
@@ -0,0 +1,28 @@
+<?php
+$link = connectToDatabase();
+if(
+    ($name = checkInput($_POST['name']) and 
+    $email = checkInput($_POST['email']) and
+    $homepage = checkInput($_POST['phone']) and
+    $comment = checkInput($_POST['message'])) != ''){
+    $insert_query = $link -> prepare("INSERT INTO Guestbook (User_id, time, Name, Email,Homepage,Comment) VALUES (NULL, CURRENT_TIMESTAMP, ?, ?, ?, ?)");
+    $insert_query->bind_param('ssss', $name, $email,$homepage,$comment);
+    $insert_query->execute();
+	header('location: index.html'); 
+}
+
+function connectToDatabase(){ // DSV databas.
+    $mysql_host='atlas.dsv.su.se';
+    $mysql_user='usr_16453842';
+    $mysql_password='453842';
+    $mysql_db='db_16453842';
+    $mysql_port = 3306;
+    return $link = @mysqli_connect($mysql_host, $mysql_user,$mysql_password, $mysql_db, $mysql_port);
+}
+
+function checkInput($input){ 
+    global $link;
+    $input = $link->escape_string(trim($input));
+    return $input;
+}
+?>
---
+++ js/main.js
@@ -0,0 +1,109 @@
+$(document).ready(function () {
+    getLanguage();
+    showLanguage();
+    /* Smooth scroll */
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (e) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            && location.hostname == this.hostname) {
+            let target = $(this.hash);
+            target = target.length ? target : '[name=' + this.hash.slice(1) + ']';
+            console.log('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                console.log(target);
+                e.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 400, 'swing', function () {
+                    window.location.hash = target;
+                });
+            }
+        }
+    });
+
+    $('#skills').hide();
+    $('#CV').hide();
+    $('#portfolio').hide();
+
+    //Add active class
+    $(".navbar li a").click(function () {
+        $(".navbar li").removeClass('active');
+        $(this).parent().addClass('active');
+    });
+
+
+    $('li.skills a').on('click', function () {
+        $('.progress-bar').css({ "width": 0 });
+        $('#home').hide();
+        $('#skills').show();
+        $('#CV').hide();
+        $('#portfolio').hide();
+        $(".progress-bar").css('width', function () {
+            return $(this).attr("aria-valuenow");
+        })
+    })
+    $('li.cv a,#cvBtn').on('click', function () {
+        $('#home').hide();
+        $('#skills').hide();
+        $('#CV').show();
+        $('#portfolio').hide();
+    })
+    $('li.portfolio a').on('click', function () {
+        $('#home').hide();
+        $('#skills').hide();
+        $('#CV').hide();
+        $('#portfolio').show();
+    })
+    $('li.homeNav a').on('click', function () {
+        $('#home').show();
+        $('#skills').hide();
+        $('#CV').hide();
+        $('#portfolio').hide();
+    })
+    //Choose language
+    $('.dropdown-menu').on('click', function (e) {
+        let laugChoosed = e.target.textContent.toLowerCase().trim();
+        console.log(laugChoosed);
+        console.log(e.target.textContent);
+        localStorage.setItem("Language", JSON.stringify(laugChoosed));
+    })
+    //Get language from localStorage
+    function getLanguage() {
+        let lang = "";
+        if (localStorage.getItem("Language") === null) {
+            lang = "en";
+        } else {
+            lang = JSON.parse(localStorage.getItem("Language"));
+        }
+        return lang;
+    }
+    function showLanguage() {
+        const langFromLocal = getLanguage();
+        console.log(langFromLocal);
+        if (langFromLocal === "sv") {
+            $('[lang="sv"').show();
+            $('[lang="en"').hide();
+            console.log("sv");
+
+        } else if (langFromLocal === "en") {
+            $('[lang="en"').show();
+            $('[lang="sv"').hide();
+            console.log("en");
+        }
+        console.log("showLanguage");
+    }
+    //Change language
+    $('#swedishLang').click(function () {
+        $('[lang="sv"').show();
+        $('[lang="en"').hide();
+    });
+    $('#englishLang').click(function () {
+        $('[lang="sv"').hide();
+        $('[lang="en"').show();
+    });
+
+    //Show msg on contace me button
+    $('#saveBtn').on('click', function () {
+        $('#contactmeModal').modal('hide');
+    });
+
+});
